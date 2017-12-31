fe0869e0406932aee4595029e03d3af9a09948e4
Jim Belcher <jimbelcher@Jims-MacBook-Pro.local>
2017-12-31 10:23:02 -0500

    initial commit

---
+++ src/main/java/com/cardstox/webgateway/WebgatewayApplication.java
@@ -0,0 +1,12 @@
+package com.cardstox.webgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgatewayApplication.class, args);
+	}
+}
